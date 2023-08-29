a64794989aa8631745eeddd28ac76f5c3a021365
vivian_pc <carohinca1997@gmail.com>
2023-08-29 10:52:57 -0500

    se añade nombre del admin y comentarios

--- webhojas/libreria/models.py
+++ webhojas/libreria/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Ventas(models.Model):
+    '''creacion tabla ventas'''
     id=models.AutoField(primary_key=True)
     nombre=models.CharField(blank=False,null=False,max_length=40)
     tipo_cliente=models.IntegerField(blank=False,null=False,max_length=2)
@@ -9,3 +10,6 @@ class Ventas(models.Model):
     subtotal=models.DecimalField(default=0.0, max_digits=8,decimal_places=2)
     neto_por_pagar=models.DecimalField(default=0.0, max_digits=8,decimal_places=2)
 
+    def __str__(self):
+        return self.nombre #Se define que el nombre del cliente es el que aparecera en los registros del admin.
+    
--- webhojas/libreria/views.py
+++ webhojas/libreria/views.py
@@ -3,16 +3,21 @@ from django.http import HttpResponse
 # Create your views here.
 
 def inicio(request):
+    '''Funcion para el portal de inicio'''
     return render (request,'html/inicio.html' )
 
 def nosotros(request):
+    '''Funcion para el final de seccion nosotros'''
     return render (request,'html/nosotros.html' )
 
 def ventas(request):
+    '''funcion para la seccion de ventas'''
     return render(request,'hojas/index.html' )
 
 def crear_venta(request):
+    '''funcion para la seccion de crear ventas'''
     return render(request,'hojas/crear.html')
 
 def editar_venta(request):
+    '''funcion para la seccion de editar ventas'''
     return render(request,'hojas/editar.html')
