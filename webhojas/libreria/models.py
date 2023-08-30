63493223d3ee9ae2d83cbd982f7357f3b5068752
vivian_pc <carohinca1997@gmail.com>
2023-08-30 12:05:20 -0500

    Validacion campo nombre(solo ingreso alfabetico)

--- webhojas/libreria/models.py
+++ webhojas/libreria/models.py
@@ -1,11 +1,19 @@
 from django.db import models
+from django.core.validators import RegexValidator
+
+
+alpha_only_validator = RegexValidator(
+    #Para asegurarse que en el campo nombre no se ingresen numeros 
+    regex=r'^[a-zA-Z]*$',
+    message='Este campo debe contener solo letras.',
+)
 
 # creacion de tbala ventas :)
 class Ventas(models.Model):
     '''creacion tabla ventas'''
     id=models.AutoField(primary_key=True)
-    nombre=models.CharField(blank=False,null=False,max_length=40)
-    tipo_cliente=models.IntegerField(blank=False,null=False,max_length=2)
+    nombre=models.CharField(blank=False,null=False,max_length=40,validators=[alpha_only_validator])
+    tipo_cliente=models.IntegerField(blank=False,null=False)
     cantidad=models.IntegerField(blank=False,null=False,default=0)
     precio_por_hoja=models.DecimalField(default=0.0, max_digits=8,decimal_places=2)
     subtotal=models.DecimalField(default=0.0, max_digits=8,decimal_places=2)
