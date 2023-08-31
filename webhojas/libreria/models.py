a723124466570a9966ee9fb3d23b348019a5caab
vivian_pc <carohinca1997@gmail.com>
2023-08-30 21:20:23 -0500

    Se condiciona campo tipocliente para que acepte solo valores de 1-4

--- webhojas/libreria/models.py
+++ webhojas/libreria/models.py
@@ -8,12 +8,20 @@ alpha_only_validator = RegexValidator(
     message='Este campo debe contener solo letras.',
 )
 
-# creacion de tbala ventas :)
+# creacion de tabla ventas :)
 class Ventas(models.Model):
     '''creacion tabla ventas'''
+    type_choices=[
+        #Opciones de tipos de usuario, si se escoge otro lanzara mensaje de error
+        (1,'1'),
+        (2,'2'),
+        (3,'3'),
+        (4,'4'),
+    ]
+
     id=models.AutoField(primary_key=True)
     nombre=models.CharField(blank=False,null=False,max_length=40,validators=[alpha_only_validator])
-    tipo_cliente=models.IntegerField(blank=False,null=False)
+    tipo_cliente=models.SmallIntegerField(blank=False,null=False,choices=type_choices,default=1)
     cantidad=models.IntegerField(blank=False,null=False,default=0)
     precio_por_hoja=models.DecimalField(default=0.0, max_digits=8,decimal_places=2)
     subtotal=models.DecimalField(default=0.0, max_digits=8,decimal_places=2)
